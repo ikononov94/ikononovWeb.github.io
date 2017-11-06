a0c634104c924424e7f153b8da1d65e829b36be5
Ivan <ikononov94@gmail.com>
2017-11-06 20:33:17 +0300

    Add files via upload

---
+++ app/assets/js/common.js
@@ -0,0 +1,20 @@
+
+
+
+var swiper = new Swiper('.swiper-container', {
+      speed: 1000,
+      effect: 'fade',
+      fadeEffect: {
+        crossFade: true
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: 'true',
+  },
+    });
+
