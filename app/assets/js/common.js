dba921f1627cd2d6b62cc3edc7180699ac644fea
IvanKononov <ikononov94@gmail.com>
2017-11-10 00:48:36 +0200

    Add files via upload

--- app/assets/js/common.js
+++ app/assets/js/common.js
@@ -1,4 +1,33 @@
+  'use strict';
 
+  var upBtn = document.getElementById('btn-toTop');
+  upBtn.style.display = "none";
+
+  function trackScroll() {
+    var scrolled = window.pageYOffset;
+
+    if (scrolled > 200) {
+      upBtn.style.display = "block";
+    }
+    else
+      upBtn.style.display = "none";
+    }
+  }
+
+  function backToTop() {
+      (function goTop() {
+
+        if (window.pageYOffset !== 0) {
+          window.scrollBy(0, -60);
+          setTimeout(goTop, 2);
+        } else {
+          upBtn.style.display = "none"
+        }
+      })();
+  }
+
+  window.addEventListener('scroll', trackScroll);
+  upBtn.addEventListener('click', backToTop);
 
 
 
@@ -18,3 +47,19 @@ var swiper = new Swiper('.swiper-container', {
         clickable: 'true',
   },
     });
+  var main = document.getElementById('scrollMain');
+  var jobs = document.getElementById('scrollJobs');
+
+  function scrollToMain() {
+      (function goMain() {
+        if(window.pageYOffset !== 0) {
+          window.scrollBy(0, -60);
+          setTimeout(goMain, 2);
+        }
+      })();
+  }
+
+
+  main.addEventListener('click', scrollToMain);
+  jobs.addEventListener('click', scrollToJobs);
+
