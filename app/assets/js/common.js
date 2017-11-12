9d4a47bc9e19334ee198fd81855006567804573e
IvanKononov <ikononov94@gmail.com>
2017-11-12 12:53:24 +0200

    Add files via upload

--- app/assets/js/common.js
+++ app/assets/js/common.js
@@ -12,7 +12,6 @@
     else
       upBtn.style.display = "none";
     }
-  }
 
   function backToTop() {
       (function goTop() {
@@ -28,6 +27,8 @@
 
   window.addEventListener('scroll', trackScroll);
   upBtn.addEventListener('click', backToTop);
+
+  
 
 
 
@@ -47,19 +48,96 @@ var swiper = new Swiper('.swiper-container', {
         clickable: 'true',
   },
     });
-  var main = document.getElementById('scrollMain');
-  var jobs = document.getElementById('scrollJobs');
+var linkNav = document.querySelectorAll('[href^="#nav"]'),
+    V = 0.4;
+for (var i = 0; i < linkNav.length; i++) {
+  linkNav[i].onclick = function(){
+    var w = window.pageYOffset,
+        hash = this.href.replace(/[^#]*(.*)/, '$1'),
+        t = document.querySelector(hash).getBoundingClientRect().top,
+        start = null;
+    requestAnimationFrame(step);
+    function step(time) {
+      if (start === null) start = time;
+      var progress = time - start;
+      var r = (t < 0 ? Math.max(w - progress/V, w + t) : Math.min(w + progress/V, w + t))
 
-  function scrollToMain() {
-      (function goMain() {
-        if(window.pageYOffset !== 0) {
-          window.scrollBy(0, -60);
-          setTimeout(goMain, 2);
-        }
-      })();
+      window.scrollTo(0,r);
+      if (r != w + t) {
+        requestAnimationFrame(step)
+      } 
+        else {
+        location.hash = hash;
+      }
+    }
+    return false;
+  }
+}
+
+window.addEventListener('scroll', function(e) {
+  var nav = document.querySelectorAll('section[id^="nav"]');
+  for (var i = 0; i < nav.length; i++) { 
+    document.querySelector('a[href="#' + nav[i].id + '"]').className=((1 >= nav[i].getBoundingClientRect().top && nav[i].getBoundingClientRect().top >= 1-nav[i].offsetHeight) ? 'focused' : '');
+    document.querySelector('a[id="pag-' + nav[i].id + '"]').className=((1 >= nav[i].getBoundingClientRect().top && nav[i].getBoundingClientRect().top >= 1-nav[i].offsetHeight) ? 'pag-active' : '');
+  }
+}, false);
+
+var burg = document.getElementById('nav__burg');
+var hidmnu = document.getElementById('hidmnu');  
+
+burg.addEventListener('click', function() {
+      hidmnu.classList.toggle('hidmnu-active');
+
+})
+
+
+
+
+    function showError(container, errorMessage) {
+    container.className = 'error';
+    var msgElem = document.createElement('span');
+    msgElem.className = "error-message";
+    msgElem.innerHTML = errorMessage;
+    container.appendChild(msgElem);
   }
 
+    function resetError(container) {
+      container.className = '';
+      if (container.lastChild.className == "error-message") {
+        container.removeChild(container.lastChild);
+      }
+    }
 
-  main.addEventListener('click', scrollToMain);
-  jobs.addEventListener('click', scrollToJobs);
+    function validate(form) {
+      var elems = form.elements;
+      var reg = /^\w{1,}@\w{1,}\.\w{2,}$/;
+
+      resetError(elems.name.parentNode);
+      if (!elems.name.value || elems.name.value == " ") {
+        showError(elems.name.parentNode, ' Укажите Ваше Имя.');
+      }
+      resetError(elems.email.parentNode);
+
+      if (!elems.email.value || elems.email.value == " ") {
+        showError(elems.email.parentNode, ' Укажите E-mail.');
+      } else if (!reg.test(elems.email.value)) {
+        showError(elems.email.parentNode, ' Не верный E-mail.');
+      }
+
+      resetError(elems.office.parentNode);
+      if (!elems.office.value || elems.office.value == " ") {
+        showError(elems.office.parentNode, ' Укажите Вашу должность.');
+      }
+
+      resetError(elems.review.parentNode);
+      if (!elems.review.value || elems.review.value == " ") {
+        showError(elems.review.parentNode, ' Отсутствует текст.');
+      }
+
+      resetError(elems.capcha.parentNode);
+      if (!elems.capcha.value || elems.capcha.value == " ") {
+        showError(elems.capcha.parentNode, ' Введите код.');
+      }
+
+    }
 
